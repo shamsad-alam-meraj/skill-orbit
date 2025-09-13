e4c2b07cc542c9928f1c7be1e024fd5be6fdc8c5
Shamsad Alam Meraj <se.fe2@ekkbaz.com>
2025-09-13 14:31:55 +0600

    tailwind set up done

---
+++ src/app/home/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-500">
+      <h1 className="text-3xl font-bold text-blue-600">SkillOrbit</h1>
+    </div>
+  );
+}
